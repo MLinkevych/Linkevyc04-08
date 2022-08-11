219f31ba77baa80b176527e25cd5193e124fca8f
Maksym Linkevych <95864524+MLinkevych@users.noreply.github.com>
2022-08-11 16:56:50 +0300

    first commit

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -1,4 +1,15 @@
 package homework1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        String phrase= scan.nextLine();
+        System.out.println(WordClass.word(phrase));
+        System.out.println(WordClass.count);
+        System.out.println(WordClass.reversedWord(phrase));
+
+    }
+
 }
